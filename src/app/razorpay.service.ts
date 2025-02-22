e237f2e1e3ef2d088e438ba02dce0125221fb6b6
Yogeshp0012 <yogeshp0012@gmail.com>
2025-02-22 09:51:27 +0530

    Migrate to angular 18

--- src/app/razorpay.service.ts
+++ src/app/razorpay.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { MedusaClientService } from './medusa-client.service';
 
